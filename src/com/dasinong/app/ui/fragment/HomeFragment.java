ce6f512ca505606205ea8df620b955a9eec331d9
刘宁ing <lxn348567248@163.com>
2015-06-06 00:24:20 +0800

    merge

--- src/com/dasinong/app/ui/fragment/HomeFragment.java
+++ src/com/dasinong/app/ui/fragment/HomeFragment.java
@@ -6,42 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-<<<<<<< HEAD
-
-import com.dasinong.app.components.HomeWeatherView;
-=======
-import android.widget.ImageView;
-import android.widget.TextView;
->>>>>>> origin/master
-
-import com.dasinong.app.R;
-
-import butterknife.ButterKnife;
-import butterknife.InjectView;
 
 public class HomeFragment extends Fragment {
 
-<<<<<<< HEAD
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-	}
-
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		
-		Button tv = new Button(getActivity());
-		tv.setText("首页");
-		tv.setTextSize(50);
-		;
-		HomeWeatherView view = new HomeWeatherView(getActivity());
-//		return inflater.inflate(R.layout.view_home_weather,container,false) ;
-		return view;
-	}
-	
-	
-	
-=======
 
 
     @Override
@@ -60,5 +27,4 @@ public class HomeFragment extends Fragment {
     }
 
 
->>>>>>> origin/master
 }
