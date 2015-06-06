7939e1e375395316c907fd36296680e717d996f5
LiamU <linmuv@163.com>
2015-06-06 18:25:47 +0800

    update

--- src/com/dasinong/app/ui/TaskDetailsActivity.java
+++ src/com/dasinong/app/ui/TaskDetailsActivity.java
@@ -1,14 +1,50 @@
 package com.dasinong.app.ui;
 
+import com.dasinong.app.R;
+import com.dasinong.app.ui.view.TopbarView;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class TaskDetailsActivity extends BaseActivity {
 
+	private TopbarView mTopbarView;
+	
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_tast_details);
 		
+		initData();
+		initView();
+		setUpView();
 		
+	}
+
+	private void initData() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void initView() {
+		mTopbarView = (TopbarView) this.findViewById(R.id.topbar);
+	}
+
+	private void setUpView() {
+		mTopbarView.setCenterText("农事指导");
+		mTopbarView.setLeftView(true, true);
+		mTopbarView.setRightText("查看全部");
+		mTopbarView.setRightClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(TaskDetailsActivity.this,TaskListActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 	
 }
--- src/com/dasinong/app/ui/TaskListActivity.java
+++ src/com/dasinong/app/ui/TaskListActivity.java
@@ -1,12 +1,38 @@
 package com.dasinong.app.ui;
 
+import com.dasinong.app.R;
+import com.dasinong.app.ui.view.TopbarView;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class TaskListActivity extends BaseActivity {
-
+private TopbarView mTopbarView;
+	
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_task_list);
+		
+		initData();
+		initView();
+		setUpView();
+	}
+
+	private void initData() {
+		
+	}
+
+	private void initView() {
+		mTopbarView = (TopbarView) this.findViewById(R.id.topbar);
+	}
+
+	private void setUpView() {
+		mTopbarView.setCenterText("全部农事");
+		mTopbarView.setLeftView(true, true);
 	}
 	
 }
