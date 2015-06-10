4b608621a3b7c72ca003465d506f4ac0479cbcae
liuningning <lxn348567248@163.com>
2015-06-11 07:26:12 +0800

    variety domain

---
+++ src/com/dasinong/app/database/variety/domain/variety.java
@@ -0,0 +1,22 @@
+package com.dasinong.app.database.variety.domain;
+
+/**
+ * Created by liuningning on 15/6/11.
+ */
+public class Variety {
+
+  public String  province;
+   public String region	;
+   public String cropOne;
+   public String cropTwo;
+   public String cropThree;
+   public String cropFour;
+   public String cropFive;
+   public String cropSix;
+   public String cropSeven;
+   public String cropEight;
+    public String  cropNine;
+    public String  cropTen	;
+    public String  cropEleven;
+    public String  cropTwelve;
+}
