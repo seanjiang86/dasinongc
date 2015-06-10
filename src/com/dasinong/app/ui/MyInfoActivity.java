be3b3e4e7d29fa115c587298691cc523f7554bca
yusonglin <songlin.yu@0071515.com>
2015-06-10 15:21:38 +0800

    upload

--- src/com/dasinong/app/ui/MyInfoActivity.java
+++ src/com/dasinong/app/ui/MyInfoActivity.java
@@ -1,7 +1,15 @@
 package com.dasinong.app.ui;
 
+import java.io.File;
+
 import com.dasinong.app.R;
 import com.dasinong.app.ui.view.TopbarView;
+import com.lidroid.xutils.HttpUtils;
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.RequestParams;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
+import com.lidroid.xutils.http.client.HttpRequest.HttpMethod;
 
 import android.os.Bundle;
 import android.view.View;
@@ -14,30 +22,30 @@ import android.widget.TextView;
 public class MyInfoActivity extends BaseActivity implements OnClickListener {
 
 	private TopbarView mTopbarView;
-	
+
 	private RelativeLayout mHeadviewLayout;
 	private ImageView mHeadviewImage;
-	
+
 	private RelativeLayout mPhoneLayout;
 	private TextView mPhoneText;
 	private Button mAuthPhoneButton;
-	
+
 	private RelativeLayout mResetPwdLayout;
-	
+
 	private RelativeLayout mNameLayout;
 	private TextView mNameText;
-	
+
 	private RelativeLayout mAddressLayout;
 	private TextView mAddressText;
-	
+
 	private RelativeLayout mHomephoneLayout;
 	private TextView mHomephoneText;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_my_info);
-		
+
 		initView();
 		setUpView();
 	}
@@ -61,7 +69,7 @@ public class MyInfoActivity extends BaseActivity implements OnClickListener {
 	private void setUpView() {
 		mTopbarView.setCenterText("个人信息");
 		mTopbarView.setLeftView(true, true);
-		
+
 		mHeadviewLayout.setOnClickListener(this);
 		mPhoneLayout.setOnClickListener(this);
 		mAuthPhoneButton.setOnClickListener(this);
@@ -69,34 +77,38 @@ public class MyInfoActivity extends BaseActivity implements OnClickListener {
 		mNameLayout.setOnClickListener(this);
 		mAddressLayout.setOnClickListener(this);
 		mHomephoneLayout.setOnClickListener(this);
+
 	}
 
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.layout_headview:
-			
+
 			break;
 		case R.id.layout_phoneNumber:
-			
+
 			break;
 		case R.id.button_auth_phone:
-			
+
 			break;
 		case R.id.layout_reset_password:
-			
+
 			break;
 		case R.id.layout_name:
-			
+
 			break;
 		case R.id.layout_address:
-			
+
 			break;
 		case R.id.layout_home_phone:
-			
+
 			break;
 		}
 	}
-	
-	
+
+	private void upload(String path) {
+	  
+	  
+	}
 }
