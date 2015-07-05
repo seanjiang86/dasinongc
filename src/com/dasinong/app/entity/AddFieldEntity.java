78a48a872f182ca0c3f2325435486816ece6ab8a
Mr-Unhappy <liu81952438@sina.com>
2015-07-06 00:59:19 +0800

    解决冲突

---
+++ src/com/dasinong/app/entity/AddFieldEntity.java
@@ -0,0 +1,29 @@
+package com.dasinong.app.entity;
+
+import java.util.List;
+import java.util.Map;
+
+public class AddFieldEntity extends BaseEntity {
+	public FieldInfo data;
+	
+	public class FieldInfo {
+		public boolean active;
+		public int currentStageID;
+		public int fieldId;
+		public String fieldName;
+		public int locationId;
+		public int monitorLocationId;
+		
+		public List natdisws;
+		public List petdisws;
+		public boolean sprayable;
+		public long startDate;
+		public List taskws;
+		public int userId;
+		public int varietyId;
+		public boolean workable;
+		public int yield;
+		
+		
+	}
+}
