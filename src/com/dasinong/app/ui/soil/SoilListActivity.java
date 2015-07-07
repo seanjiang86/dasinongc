8544a4b4b057cc3656358c46955c20717cafa120
Ming <Ming@192.168.1.127>
2015-07-07 21:34:45 +0800

    完善测土HTML页面

--- src/com/dasinong/app/ui/soil/SoilListActivity.java
+++ src/com/dasinong/app/ui/soil/SoilListActivity.java
@@ -52,8 +52,6 @@ public class SoilListActivity extends SoilBaseActivity implements AdapterView.On
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-        String url = "";
         Intent intent = new Intent();
         switch (position) {
             case 0:
@@ -63,24 +61,30 @@ public class SoilListActivity extends SoilBaseActivity implements AdapterView.On
             	startActivity(intent);
                 break;
             case 1:
-                url = "";
+            	intent.setClass(this, RegisterServiceActivity.class);
+            	intent.putExtra("url", "SamplingExample.htm");
+            	intent.putExtra("title", "测土配方报告示例");
+            	startActivity(intent);
                 break;
             case 2:
-                url = "";
-                break;
-            case 3:
-                url = "";
-                break;
-            case 4:
             	intent.setClass(this, RegisterServiceActivity.class);
             	intent.putExtra("url", "SamplingNotice.htm");
             	intent.putExtra("title", "采样须知");
             	startActivity(intent);
                 break;
-            default:
-                url = "";
+            case 3:
+            	
+            	intent.setClass(this, RegisterServiceActivity.class);
+            	intent.putExtra("url", "SamplingUnscramble.htm");
+            	intent.putExtra("title", "测土报告解读");
+            	startActivity(intent);
                 break;
-
+            case 4:
+            	intent.setClass(this, RegisterServiceActivity.class);
+            	intent.putExtra("url", "SamplingStation.htm");
+            	intent.putExtra("title", "哪里可以测土？");
+            	startActivity(intent);
+                break;
         }
     }
 
