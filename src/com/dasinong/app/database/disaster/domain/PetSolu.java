bfc3e469b70835a4fe4cc4d743a4d5ef4463bae8
liuningning <lxn348567248@163.com>
2015-07-15 08:28:00 +0800

    ci

--- src/com/dasinong/app/database/disaster/domain/PetSolu.java
+++ src/com/dasinong/app/database/disaster/domain/PetSolu.java
@@ -6,9 +6,24 @@ import java.io.Serializable;
  * Created by liuningning on 15/6/2.
  */
 public class PetSolu implements Serializable {
+    /**
+     * `petSoluId` int(10) unsigned NOT NULL AUTO_INCREMENT,
+     `petSoluDes` varchar(2000) NOT NULL,
+     `petDisSpecId` int(10) unsigned NOT NULL,
+     `providedBy` varchar(50) DEFAULT NULL,
+     `isRemedy` tinyint(1) DEFAULT '1',
+     `isCPSolu` tinyint(1) DEFAULT '1',
+     `rank` int(4) DEFAULT NULL,
+     `subStageId` varchar(50) NOT NULL DEFAULT '',
+     */
     public int  petSoluId;
     public String petSoluDes;
-    public int isCure;
+    public int petDisSpecId;
+    public String providedBy;
+    public int isRemedy;
     public int isCPSolu;
-    public long petDisSpecId;
+    public int rank;
+    public String subStageId;
+
+
 }
--- src/com/dasinong/app/database/task/domain/TaskSpec.java
+++ src/com/dasinong/app/database/task/domain/TaskSpec.java
@@ -4,6 +4,12 @@ package com.dasinong.app.database.task.domain;
  * Created by liuningning on 15/6/6.
  */
 public class TaskSpec {
+    /**
+     *   `taskSpecId` int(10) unsigned NOT NULL AUTO_INCREMENT,
+     `taskSpecName` varchar(30) NOT NULL,
+     `subStageId` int(10) unsigned NOT NULL,
+     `type` varchar(10) DEFAULT NULL,
+     */
     public int taskSpecId;
     public String taskSpecName;
     public int subStage;
