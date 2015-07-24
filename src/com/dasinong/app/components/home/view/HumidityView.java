2c670ec01875f240b9cc45595daa2ef1e999b28f
刘宁ing <lxn348567248@163.com>
2015-07-24 16:48:12 +0800

    Update HumidityView.java
    fix 24 hour data is null

--- src/com/dasinong/app/components/home/view/HumidityView.java
+++ src/com/dasinong/app/components/home/view/HumidityView.java
@@ -19,6 +19,7 @@ import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
+import java.util.Iterator; 
 
 /**
  * Created by  lxn on 15/6/5.
@@ -68,6 +69,16 @@ public class HumidityView extends LinearLayout {
             return;
         }
 
+       
+        Iterator<WeatherEntity.Hours> iterator = hoursList.iterator();
+        while(iterator.hasNext()){
+            WeatherEntity.Hours item = iterator.next();
+            if(item==null){
+                iterator.remove();
+            }
+            
+        }
+        
         int size = hoursList.size();
         removeAllViews();
         for (int i = 0; i < size; i++) {
