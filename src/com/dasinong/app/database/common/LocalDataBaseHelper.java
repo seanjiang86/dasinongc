86080b4e80d6d06d58383f022b76844f1b4a5fee
lxn348567248 <lxn348567248@163.com>
2015-07-30 20:33:24 +0800

    db force update

--- src/com/dasinong/app/database/common/LocalDataBaseHelper.java
+++ src/com/dasinong/app/database/common/LocalDataBaseHelper.java
@@ -9,11 +9,11 @@ import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
  */
 public class LocalDataBaseHelper extends SQLiteAssetHelper {
     private static final String DATABASE_NAME = "dns.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
 
     public LocalDataBaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
-
+        setForcedUpgrade();
     }
 
 }
